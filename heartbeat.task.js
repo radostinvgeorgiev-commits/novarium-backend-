d36ee81f86908f651520d1682675a2cc3fe13525
radostinvgeorgiev-commits <radostin.v.georgiev@gmail.com>
2025-12-02 16:56:48 -0800

    Delete heartbeat.task.js

--- heartbeat.task.js
+++
@@ -1,8 +0,0 @@
-// agent/tasks/heartbeat.task.js – Agent heartbeat
-
-module.exports = {
-  name: "heartbeat",
-  async run() {
-    console.log(`💓 AGENT HEARTBEAT: ${new Date().toISOString()}`);
-  }
-};
